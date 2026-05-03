420dc8361dfcf03bcbb68c3813281230eab2095f
pulkit2412 <pulkit3124@gmail.com>
2026-05-03 17:39:08 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
-
+const BASE_URL = "https://resqvoice.onrender.com";
 // -------- REGISTER --------
 async function register() {
     const email = document.getElementById("email").value;
